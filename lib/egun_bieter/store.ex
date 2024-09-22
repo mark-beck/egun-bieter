8656e1e081b82225a633f5a719856e968723352b
Mark Beck <>
2024-09-22 16:18:44 +0200

    added file persistance

--- lib/egun_bieter/store.ex
+++ lib/egun_bieter/store.ex
@@ -1,7 +1,13 @@
 defmodule EgunBieter.Store do
 
   def start do
-    :ets.new(:articles, [:named_table, :public])
+    if (File.exists?("store.data")) do
+      :ets.file2tab(~c"store.data")
+    else
+      :ets.new(:articles, [:named_table, :public])
+    end
+
+
   end
 
   def get_all do
@@ -17,14 +23,26 @@ defmodule EgunBieter.Store do
 
   def update art do
     :ets.insert(:articles, {art.id, art})
+    backup_data()
   end
 
   def add art do
     :ets.insert_new(:articles, {art.id, art})
+    backup_data()
   end
 
   def remove id do
     :ets.delete(:articles, id)
+    backup_data()
   end
 
+  defp backup_data do
+    # first, rename old backup if it exists
+    if File.exists?("store.data") do
+      File.cp("store.data", "store.backup")
+      File.rm("store.data")
+    end
+
+    :ets.tab2file(:articles, ~c"store.data")
+  end
 end
